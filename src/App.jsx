19c416e15c82b82a1c988d8af94afad50d587214
Connor Tracy <connortracy15@gmail.com>
2021-09-17 19:24:22 -0400

    working server

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Star Nosed Moles!!!</h1>
+      </div>
+    )
+  }
+}
+
+
+// export default = App
