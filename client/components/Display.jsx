6b7cefa98086a803d78dbc8981434366cc22a43b
mkalaizic <kalaizicmercedes@gmail.com>
2021-09-18 18:20:02 -0400

    changes Mercedes Sat 6pm

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Games from './Games.jsx'
-// import Home from './Home.jsx'
+import img from './images.png';
+
 
 export default function Display() {
   const [show, setShow] = useState("home");
@@ -16,33 +17,43 @@ export default function Display() {
   
   if(show==="home"){
     return (
+      <div>
+        {/* <img src={img} id="Mole"></img> */}
         <div className= "Home">
+
         <h1>Learning JavaScript</h1>
-        {/* <img src= {img} ></img> */}
         <h2>with Mole</h2>
+
+         
           <button id= "homeBtn" onClick={()=>{
             setGameName("games1");
             setShow('games');
-          } }>Game 1</button>
-          <button id= "homeBtn" onClick={()=>{
+          } }>Game 1 <img src={img} id="MoleInBtn"></img> </button>          
+
+        <button id= "homeBtnBlocked" onClick={()=>{
             setGameName("games2");
             setShow('games');
           } }>Game 2</button>
-          <button id= "homeBtn" onClick={()=>{
+
+        <button id= "homeBtnBlocked" onClick={()=>{
             setGameName("games3");
             setShow('games');
           } }>Game 3</button>
-          <button id= "homeBtn" onClick={()=>{
+
+        <button id= "homeBtnBlocked" onClick={()=>{
             setGameName("games4");
             setShow('games');
           } }>Game 4</button>
+
+           
+        </div>
         </div>
         )
         }
     else{
     return (
         <div>
-          <button onClick={changeToHome}>Home</button>
+          <button onClick={changeToHome} id="backHome">Home page</button>
           <Games gameNumber={gameName}/>
         </div>
       )
