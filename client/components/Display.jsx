d88eb8ea75066553e2c39afd97e06791e4bfef9f
mkalaizic <kalaizicmercedes@gmail.com>
2021-09-18 16:27:10 -0400

    still waiting for mongo data

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -1,38 +1,52 @@
 import React, { useState, useEffect } from 'react'
 import Games from './Games.jsx'
-import Home from './Home.jsx'
+// import Home from './Home.jsx'
 
-export default function Display(props) {
+export default function Display() {
   const [show, setShow] = useState("home");
+  const [gameName, setGameName] = useState('no game')
 
-  function changeState(){
-    setShow("games");
-  }
-  
   function changeToHome(){
     setShow("home");
   }
   
   useEffect(() => {
-    console.log('show State changed')
-  },[show])
+    console.log('show State changed'+ gameName)
+  },[gameName])
   
   if(show==="home"){
     return (
-        <div>
-          <Home render={changeState}/>
+        <div className= "Home">
+        <h1>Learning JavaScript</h1>
+        {/* <img src= {img} ></img> */}
+        <h2>with Mole</h2>
+          <button id= "homeBtn" onClick={()=>{
+            setGameName("games1");
+            setShow('games');
+          } }>Game 1</button>
+          <button id= "homeBtn" onClick={()=>{
+            setGameName("games2");
+            setShow('games');
+          } }>Game 2</button>
+          <button id= "homeBtn" onClick={()=>{
+            setGameName("games3");
+            setShow('games');
+          } }>Game 3</button>
+          <button id= "homeBtn" onClick={()=>{
+            setGameName("games4");
+            setShow('games');
+          } }>Game 4</button>
         </div>
-      )
-  }
-  else{
+        )
+        }
+    else{
     return (
         <div>
           <button onClick={changeToHome}>Home</button>
-          <Games/>
+          <Games gameNumber={gameName}/>
         </div>
       )
   }
-  
 }
 
 
