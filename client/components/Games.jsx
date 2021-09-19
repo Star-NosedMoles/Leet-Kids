d192d11d2d3e129bd9516d86c26083bd3b05d16c
joelparkk <joelpark97@gmail.com>
2021-09-19 19:13:50 -0400

    level lock and unlock system working

--- client/components/Games.jsx
+++ client/components/Games.jsx
@@ -2,28 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Games(props) {
-  //the state will be given by the button that was pressed
-  // const [level,setLevel] =
-  // const [promptType, setPromptType] = useState('game1');
-  const [prompts, setPrompts] = useState([]);
-  const [level, setLevel] = useState(1);
-  const [victory, setVictory] = useState('');
+  //declaring states 
+  const [prompts, setPrompts] = useState('');
+  const [level, setLevel] = useState(props.currentLevel);
+  const [gameActive, setGameActive] = useState(false);
   
   function levelUp(value){
-    
-    // console.log(prompts);
-    // console.log(value);
-    if(value === prompts[level - 1]){
+    if(value === prompts){
       setLevel(prevLevel => prevLevel + 1);
       document.getElementById('userAnswer').value = ''
     } else {
       document.getElementById('userAnswer').value = ''
     }
   }
-  function winCondition()  {
-    setVictory('Congratulations!')
-  }
-  
+  // useEffect(() => {
+  //   props.playerLevel = level
+  // },[victory])
+
   //for now, we want to increment our state Level by one for every time Submit is clicked
   //we also want the appropriate prompt to appear on the page depending on the current state of the players level
   //component did mount
@@ -31,45 +26,41 @@ export default function Games(props) {
   //component did unmount
   useEffect(() => {
       // axios.post(`/api/${props.gameNumber}`)
-      
       axios.get(`/api/${props.gameNumber}`) // "games1"
         // axios.get(`/api`)
+        // .then((res)=> console.log(res.data))
         .then((res) => {
-          const cleanResult = res.data.map((el) => {
+          let cleanResult;
+          res.data.forEach((el) => {
             if (el.level === level) {
-              return el.prompt;
+              cleanResult= el.prompt;
             }
-          });
-          setPrompts(cleanResult);
-          console.log(prompts)
-          // if(prompts[level] === undefined) setPrompts('Game Locked!')
-          //setPrompts(cleanResult);
-          return 'Game Locked!';
-        });
-        if (prompts[prompts.length - 1] !== undefined) {setVictory('Victory')}
+          })
+          if(cleanResult === undefined && gameActive == true){
+            //create a better victory page
+            setPrompts('Victory!');
+            props.sendLevel(level);
+            props.unlockGame(props.gameNumber)
+            setTimeout(() => props.backHome('home'),2500)
+          }
+          else if(cleanResult===undefined && gameActive === false) {
+            props.backHome('home');
+          }
+          else{
+            setPrompts(cleanResult);
+            setGameActive(true);
+          } 
+        })
   }, [level])
 
-  // useEffect(() => {
-    
-  // },[level])
+  
   return (
     <div className="gamePage">
       <h3>{props.gameNumber}</h3>
       <h3>{level}</h3>
-      <h3>{victory}</h3>
       <p id="prompts">{prompts}</p>
       <input id = 'userAnswer' type = 'text' placeholder = "your answer!"></input>
       <button id="submit" onClick={() => levelUp(document.getElementById('userAnswer').value)}>submit</button>
     </div>
   );
 }
-
-// function Test(props){
-//   props.gameNumber;
-//    return;
-//   //  (
-// //     <div>HI</div>
-// //   )
-// }
-
-// export default {Games, Test}
