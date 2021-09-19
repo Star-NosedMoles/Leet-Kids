46bcd1c57ef061fa9dac846d3d012271f40515c1
joelparkk <joelpark97@gmail.com>
2021-09-18 21:56:59 -0400

    game1 we got prompts to render based on level, and also checks for correct answer,no victory screen,level does not persist upon going back to home page

--- server/routers/api.js
+++ server/routers/api.js
@@ -6,6 +6,14 @@ const router = express.Router();
 //leetKidsController,
 //we need a route that handles grabbing the questions
     //***me may need multiple routes to handle different difficulties of questions (ie, if we have 4 levels we would make 4 routes to access the appropriate database)
+// router.get('/',leetKidsController.getQuestions, (req, res) => {
+    
+//     res.status(200).json(res.locals.prompt)
+// });
+
+
+
+
 router.get('/',leetKidsController.getQuestions, (req, res) => {
     
     res.status(200).json(res.locals.prompt)
